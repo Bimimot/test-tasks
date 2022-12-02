4c183b00339a06536f46a2fc70840569734a1863
Stepan Popov <bimimot@yandex.ru>
2022-12-02 22:57:31 +0100

    Create NodeList JS

---
+++ node_list/create-node-list.js
@@ -0,0 +1,31 @@
+class ListNode {
+  constructor(value, next = null) {
+    this.value = value;
+    this.next = next;
+  }
+
+  toString(callback) {
+    return callback ? callback(this.value) : `${this.value}`;
+  }
+};
+
+const makeList = (arr) => {
+  let res;
+  arr
+    .reverse()
+    .forEach((item, i) => (res = new ListNode(+item, i === 0 ? null : res)));
+  return res;
+};
+
+console.log(makeList([1, 2, 3]));
+
+// {
+//     value: 1,
+//     next: {
+//         value: 2,
+//         next: {
+//             value: 3,
+//             next: null
+//         }
+//     }
+// }
